de7f8e7d8c5e5fda074ac8d474f57fd2c5e519b6
Lightening Gibson <lighteningibson@gmail.com>
2022-10-06 16:57:01 -0500

    commit tree a16af3c1817ee9c9d88d2022d5559b8e47858b50

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+
+
+
+function submitData(name, email) {
+
+  const configurationObject = {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({name, email}),
+  }
+
+
+  return fetch('http://localhost:3000/users', configurationObject)
+  .then(response => response.json())
+  .then(object => {
+    document.querySelector("body").innerHTML = object.id;})
+  .catch(error => { document.querySelector("body").innerHTML = error.message
+
+  })
+}
